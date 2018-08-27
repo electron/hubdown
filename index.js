217f806190c94a67063c431d9f10f4d65c515ae1
Zeke Sikelianos <zeke@sikelianos.com>
2018-08-26 18:32:29 -0700

    feat: add optional leveldb cache

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const autolinkHeadings = require('remark-autolink-headings')
 const inlineLinks = require('remark-inline-links')
 const grayMatter = require('gray-matter')
 const pify = require('pify')
+const hasha = require('hasha')
 
 const renderer = remark()
   .use(slug)
@@ -16,6 +17,7 @@ const renderer = remark()
   .use([hljs, html], {sanitize: false})
 
 module.exports = async function hubdown (markdownString, opts = {}) {
+  const hash = hasha(markdownString)
   const defaults = {
     frontmatter: false
   }
@@ -24,6 +26,15 @@ module.exports = async function hubdown (markdownString, opts = {}) {
   let data = {}
   let content = markdownString
 
+  // check the cache for preprocessed markdown
+  if (opts.cache) {
+    const existing = await opts.cache.get(hash).catch(err => {
+      console.debug(err)
+      return null
+    })
+    if (existing) return existing
+  }
+
   if (opts.frontmatter) {
     const parsed = grayMatter(markdownString)
     data = parsed.data
@@ -31,5 +42,10 @@ module.exports = async function hubdown (markdownString, opts = {}) {
   }
 
   const md = await pify(renderer.process)(content)
-  return Object.assign(data, {content: md.contents})
+  Object.assign(data, {content: md.contents})
+
+  // save processed markdown in cache
+  if (opts.cache) await opts.cache.put(hash, data)
+
+  return data
 }
